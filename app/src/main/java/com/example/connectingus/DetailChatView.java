315977fbb0671a3edf769ca25ab88240660690eb
Vivek Utture <63404919+vivekutture@users.noreply.github.com>
2022-04-06 15:34:04 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/connectingus/DetailChatView.java
@@ -0,0 +1,52 @@
+package com.example.chats;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DetailChatView extends AppCompatActivity {
+
+    ImageView ivProf;
+    TextView tvUname;
+
+    User user;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detailchatview);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);  // back button
+
+        getSupportActionBar().setDisplayShowTitleEnabled(false);  // hide Title
+
+        getSupportActionBar().setDisplayShowCustomEnabled(true); // Custom Appbar
+        LayoutInflater layoutInflater=(LayoutInflater)this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view=layoutInflater.inflate(R.layout.custom_appbar,null);
+        getSupportActionBar().setCustomView(view);
+
+        ivProf=findViewById(R.id.iv_prof_pic);
+        tvUname=findViewById(R.id.tv_uname);
+
+        Intent intent=getIntent();
+        if(intent.getExtras()!=null)
+        {
+            user= (User) intent.getSerializableExtra("user");
+            ivProf.setImageResource(user.getImageId());
+            tvUname.setText(user.getName());
+        }
+    }
+
+    //Menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.detail_chat_menus,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+}
